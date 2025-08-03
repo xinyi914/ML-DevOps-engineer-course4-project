6df0079fd93887572fbfad1ae71a8a07bedb06fb
xinyi914 <121588273+xinyi914@users.noreply.github.com>
2025-08-03 19:43:48 -0400

    Update test_model_code.py

--- starter/starter/test_model_code.py
+++ starter/starter/test_model_code.py
@@ -9,7 +9,7 @@ from sklearn.ensemble import RandomForestClassifier
 from ml.model import train_model, compute_model_metrics,inference
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))
 
 @pytest.fixture(scope="session")
 def params():
